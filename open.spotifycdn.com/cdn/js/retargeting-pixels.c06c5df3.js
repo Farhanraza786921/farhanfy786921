eb62791ef6158af237b45f1299605d5647d19705
Farhanraza786921 <152185237+Farhanraza786921@users.noreply.github.com>
2024-02-14 12:54:13 +0530

    first commit

---
+++ open.spotifycdn.com/cdn/js/retargeting-pixels.c06c5df3.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+(function (w) {
+  var configEl = document.getElementById('config');
+  if (!configEl) return;
+  var config = JSON.parse(configEl.innerHTML);
+  if (!config.retargetingPixels) return;
+
+  Object.entries(config.retargetingPixels).forEach(function (pixel) {
+    var pixelPlatform = pixel[0];
+    var pixelId = pixel[1];
+
+    switch (pixelPlatform) {
+      case 'twitter':
+        w.__TWITTER_PIXEL_ID__ = pixelId;
+        break;
+
+      case 'pinterest':
+        w.__PINTEREST_PIXEL_ID__ = pixelId;
+        break;
+
+      case 'snapchat':
+        w.__SNAPCHAT_PIXEL_ID__ = pixelId;
+        break;
+
+      default:
+        break;
+    }
+  });
+})(window);
---
+++ www.google.com/recaptcha/enterprise/webworker.js
@@ -0,0 +1,1 @@
+importScripts('https://www.gstatic.com/recaptcha/releases/x5WWoE57Fv0d6ATKsLDIAKnt/recaptcha__en.js');
